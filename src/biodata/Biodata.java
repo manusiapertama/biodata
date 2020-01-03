039f61360f3fc03ec1bb0c023c509f13da03a526
adam raihan <adam@gmail.com>
2020-01-03 09:52:15 +0700

    first commit

---
+++ src/biodata/Biodata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author adam
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("// BIODATA ADAM RAIHAN //");
+        System.out.println("[+] NIM : 1820010004");
+        System.out.println("[+] Nama Lengkap : Adam Raihan");
+        System.out.println("[+] Nama Panggilan : Adam");
+        System.out.println("[+] Tempat, Tanggal lahir : Jakarta, 1 Maret 2001");
+        System.out.println("[+] Jenis Kelamin : Laki - Laki");
+        System.out.println("[+] Agama : Islam");
+        System.out.println("[+] Suku : Betawi");
+        System.out.println("[+] Hobi : Musik");
+        System.out.println("[+] Cita-cita : Pengusaha");
+        System.out.println("[+] Alamat Rumah : Jl.Raya Bogor KM.28");
+        System.out.println("[+] No HP : 081768492345");
+        System.out.println("[+] Email : adam.raihan2000@gmail.com");
+    }
+}
